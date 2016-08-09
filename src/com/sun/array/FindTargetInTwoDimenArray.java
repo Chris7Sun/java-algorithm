5ed3ff510e37cb3a932a096cde92572bef322508
Chris7Sun <380714865@qq.com>
2016-08-09 23:16:12 +0800

    update FindTargetInTwoDimenArray

--- src/com/sun/array/FindTargetInTwoDimenArray.java
+++ src/com/sun/array/FindTargetInTwoDimenArray.java
@@ -13,6 +13,13 @@ public class FindTargetInTwoDimenArray {
         int row = array.length - 1;
         int column = 0;
 
+        for(int i = 0; i <= row; i++){
+            return binarySearch(array[i],0,array[i].length-1,target);
+        }
+        return false;
+
+        /*
+        //从左下角开始找，比它小的行减1，比它大的列加1
         while(row >= 0 && column < array[row].length){
             if(target == array[row][column])
                 return true;
@@ -23,5 +30,23 @@ public class FindTargetInTwoDimenArray {
             }
         }
         return false;
+        */
+    }
+    public static boolean binarySearch(int[] arr, int low, int high, int target){
+        int mid = low + (high - low)/2;
+        if(low < 0 || high < 0)
+            return false;
+        if(arr[mid] == target)
+            return true;
+        else if(target > arr[mid]){
+            return binarySearch(arr,mid+1,high,target);
+        }else if(target < arr[mid]){
+            return binarySearch(arr,low,mid-1,target);
+        }else
+            return false;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9,11,213,435,2345,5789};
+        System.out.println(binarySearch(arr,0,arr.length-1,0));
     }
 }
