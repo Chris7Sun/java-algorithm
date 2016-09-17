2f560604cac7b3965219e912b7cbb6cdecf0d79f
Chris7Sun <380714865@qq.com>
2016-09-17 23:03:18 +0800

    add RotateString

---
+++ src/com/sun/string/RotateString.java
@@ -0,0 +1,24 @@
+package com.sun.string;
+
+/**
+ * Created by Chris on 2016/9/8.
+ * 旋转字符串:给定一个字符串和一个偏移量，根据偏移量旋转字符串(从左向右旋转)
+ * 样例
+ *对于字符串 "abcdefg".
+
+ *offset=0 => "abcdefg"
+ *offset=1 => "gabcdef"
+ *offset=2 => "fgabcde"
+ *offset=3 => "efgabcd"
+ *
+ */
+public class RotateString {
+    public static void main(String[] args) {
+
+    }
+    public static void rotateString(char[] str, int offset) {
+        if (offset == 0)
+            return;
+    }
+
+}
